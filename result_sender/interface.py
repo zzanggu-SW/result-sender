00b098d68b3c4664700ee6c3087830696f875f33
zzanggu-SW <gyeongmin951@aiofarm.co.kr>
2024-08-26 11:53:07 +0900

    update : result_sender tp_sender test

--- result_sender/interface.py
+++ result_sender/interface.py
@@ -1,21 +1,18 @@
-from result_sender.interface import ResultInterface
-
-
-class ResultSender(ResultInterface):
+class ResultInterface:
     def __init__(self):
         self.logger = None
 
     def run(self):
-        pass
+        raise NotImplementedError("This method should be implemented by subclasses.")
 
     def check_valid_config(self):
         """설정한 값이 해당 SerialResultSender와 유효한지 확인합니다"""
-        pass
+        raise NotImplementedError("This method should be implemented by subclasses.")
 
     def create_default_config(self):
         """현장에서 사용하는 틀을 생성합니다"""
-        pass
+        raise NotImplementedError("This method should be implemented by subclasses.")
 
     def return_arduino_sketch(self):
         """현장에서 사용하는 아두이노 코드를 반환합니다"""
-        pass
+        raise NotImplementedError("This method should be implemented by subclasses.")
