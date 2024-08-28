ad0825845ea8baaa96d4b04b8583fa939773ea4f
zzanggu-SW <gyeongmin951@aiofarm.co.kr>
2024-08-28 10:20:49 +0900

    fix : classmethod

--- result_sender/all_senders/tp_sender.py
+++ result_sender/all_senders/tp_sender.py
@@ -6,20 +6,20 @@ class ResultSender(ResultInterface):
         super().__init__()
 
     @classmethod
-    def run():
+    def run(cls):
         pass
 
     @classmethod
-    def check_valid_config():
+    def check_valid_config(cls):
         """설정한 값이 해당 SerialResultSender와 유효한지 확인합니다"""
         pass
 
     @classmethod
-    def create_default_config():
+    def create_default_config(cls):
         """현장에서 사용하는 틀을 생성합니다"""
         pass
 
     @classmethod
-    def get_arduino_sketch():
+    def get_arduino_sketch(cls):
         """현장에서 사용하는 아두이노 코드를 반환합니다"""
         pass
